8d02130729b9c492cf50e60e944dd4e641b3b496
Emilio Lehoucq <50888924+emiliolehoucq@users.noreply.github.com>
2024-10-23 08:33:44 -0500

    Add files via upload

---
+++ test_undetected-chromedriver_gh_actions.py
@@ -0,0 +1,30 @@
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+driver = uc.Chrome()
+
+driver.get("https://www.higheredjobs.com/faculty/search.cfm?JobCat=62")
+
+logging.info(driver.page_source)
+
+# get all the links
+links = driver.find_elements(By.TAG_NAME, "a")
+urls = [link.get_attribute("href") for link in links]
+# filter URLs to those that have "details.cfm?JobCode=" in them
+urls = [url for url in urls if url and "details.cfm?JobCode=" in url]
+logging.info(f"Number of URLs found: {len(urls)}")
+logging.info(f"First 5 URLs: {urls[:5]}")
+# this seems to get all the URLs across all pages--not just the first page
+
+url_job = urls[5]
+driver.get(url_job)
+
+logging.info(driver.page_source)
+
+driver.quit()
+
+logging.info("script run until the end")
